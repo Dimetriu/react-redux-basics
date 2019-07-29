63c6977d09ea8db22968ea8c9bc43e09825d160f
Dimetriu <dima.rashidov@gmail.com>
2019-07-29 13:13:56 +0300

    change ui for User

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -11,7 +11,7 @@ export default function App() {
   return (
     <main className="App">
       <Suspense fallback={UserContentLoader}>
-        <User />
+        <User userName="Dimas" />
       </Suspense>
     </main>
   );
--- src/components/User/index.jsx
+++ src/components/User/index.jsx
@@ -3,23 +3,32 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const useStyles = makeStyles({
   root: {
-    margin: 'auto'
+    alignSelf: 'center'
   },
 });
 
-export default function User() {
+export default function User({ userName }) {
   const classes = useStyles();
 
   return (
     <section className={classes.root}>
       <CssBaseline />
       <Container maxWidth="sm">
-        <Typography variant="h5" gutterBottom>
-          User Component
+        <Typography variant="h3" gutterBottom>
+          The User Page
         </Typography>
+        <Card>
+          <CardContent>
+            <Typography variant="h5" >
+              {userName}
+            </Typography>
+          </CardContent>
+        </Card>
       </Container>
     </section>
   );
